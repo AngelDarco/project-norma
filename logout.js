3339df06c459bf9053553b0f50ffa20adc76c687
angeldarco <angel92-eme@hotmail.com>
2022-11-21 05:55:50 +0000

    version 3.0

---
+++ logout.js
@@ -0,0 +1,18 @@
+const logout = ()=>{
+    document.querySelector('.salir').addEventListener('click', () => {
+         fetch('./logout.php')
+             .then(res => res.json())
+             .then(data => {
+                 console.log(data);
+                 Swal.fire({
+                     position: 'center',
+                     icon: 'error',
+                     title: 'Debes Iniciar Sesion Primero',
+                     showConfirmButton: false,
+                     timer: 1000
+                 });
+             })
+             .catch(err => console.log(err))
+     })
+}
+export default logout;
