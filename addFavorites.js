954fc7e2139c0bc875f99c0bca17e2e68d20aa08
Angel Darco <angel92-eme@hotmail.com>
2023-02-10 01:34:07 -0500

    clean code like button

---
+++ addFavorites.js
@@ -0,0 +1,80 @@
+export default function addFavorites() {
+  const observer = new MutationObserver(like);
+  observer.observe(document.querySelector('.img__main'),
+    { attributes: false, childList: true, subtree: false })
+  }
+
+function like() {
+  document.querySelectorAll('.heart')
+  .forEach(e => {
+    e.addEventListener('click', add, true);
+  })
+}
+
+function add(z){
+  const user = window.localStorage.getItem('session');
+  const id = z.target.parentNode.parentNode.parentNode.childNodes[1].childNodes[1].childNodes[5].innerHTML;
+
+  if (user) {
+    // not like
+    if(!z.target.classList.contains('like')) {
+      agregarFavoritos(id, user);      
+      z.target.classList.replace('far', 'fas');
+      z.target.classList.add('like');
+    } else {
+      // like
+      eliminarFavoritos(id,user)
+      z.target.classList.replace('fas', 'far');
+      z.target.classList.remove('like');
+    }
+  } else
+    Swal.fire({
+      position: 'center',
+      icon: 'warning',
+      title: 'You must be loged first',
+      showConfirmButton: false,
+      timer: 500
+    })
+}
+    
+  /* Mostrar Favoritos */
+  function agregarFavoritos(id, user) {
+    const data = JSON.parse(window.localStorage.getItem(user));
+    const datalikes = { data: [] }
+
+    if (!data) {
+      datalikes.data.push(id)
+      window.localStorage.setItem(user, JSON.stringify(datalikes));
+    } else {
+      datalikes.data.push(...data.data, id);
+      window.localStorage.setItem
+        (user, JSON.stringify(
+          datalikes
+        ));
+    }
+
+    Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'Guardado',
+      showConfirmButton: false,
+      timer: 500
+    })
+  };
+
+  /* Eliminar Favoritos */
+  function eliminarFavoritos(id,user) {
+    const data = JSON.parse(window.localStorage.getItem(user));
+    const datadislikes = { data: []}
+    const res = data.data.filter(itm => itm != id )
+    datadislikes.data = res
+    window.localStorage.setItem(user, JSON.stringify(datadislikes));
+
+    Swal.fire({
+      position: 'center',
+      icon: 'warning',
+      title: 'Borrado',
+      showConfirmButton: false,
+      timer: 500
+    })
+  };
