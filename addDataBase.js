87b8ecc1ba889ed5d798165e1324385971db43b9
Angel Darco <angel92-eme@hotmail.com>
2023-02-14 13:33:06 -0500

    add events cards buttons file

---
+++ addDataBase.js
@@ -0,0 +1,13 @@
+export default function addDataBase(funct,className) {
+    const observer = new MutationObserver(call);
+    observer.observe(document.querySelector('.img__main'),
+      { attributes: false, childList: true, subtree: false });
+
+    function call(){
+        document.querySelectorAll(className)
+    .forEach(e => {
+      e.addEventListener('click',funct, true);
+    })
+    }
+  }
+  
