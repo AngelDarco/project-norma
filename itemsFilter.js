1bd914c822d3473da4ee92ddec5b3a65f86c18b5
Angel Darco <angel92-eme@hotmail.com>
2023-03-04 00:51:58 -0500

    items filter updated

--- itemsFilter.js
+++ itemsFilter.js
@@ -1,34 +1,29 @@
 import removeEvents from "./removeEvents.js";
-import { loadObserver, fetchData, dataLocalStorage } from './mostrarCards.js';
+import { loadObserver, fetchData, dataLocalStorage } from "./mostrarCards.js";
 
 export default async function itemsFilter() {
-  const data = await Promise.resolve(fetchData()).then(data => data);
-  const filtros = document.querySelectorAll('.filtro');
+  const data = await Promise.resolve(fetchData()).then((data) => data);
+  const filtros = document.querySelectorAll(".filtro");
   const { removeCar, removeLikes, removeFilters } = removeEvents();
   const { likeData, carData } = dataLocalStorage();
 
-  filtros.forEach(e => {
-
-    e.addEventListener('click', (e) => {
-      if (e.target.classList.contains('damas')) {
-        let womans = data.filter(item => item.genero == 'Mujer');
+  filtros.forEach((e) => {
+    e.addEventListener("click", (e) => {
+      if (e.target.classList.contains("damas")) {
+        let womans = data.filter((item) => item.genero == "Mujer");
         loadObserver(womans, likeData, carData);
-
-      } else if (e.target.classList.contains('caballeros')) {
-        let mens = data.filter(item => item.genero == 'Hombre');
+      } else if (e.target.classList.contains("caballeros")) {
+        let mens = data.filter((item) => item.genero == "Hombre");
         loadObserver(mens, likeData, carData);
-
-      } else if (e.target.classList.contains('ninos')) {
-        let kids = data.filter(item => item.genero == 'Niño');
+      } else if (e.target.classList.contains("ninos")) {
+        let kids = data.filter((item) => item.genero == "Niño");
         loadObserver(kids, likeData, carData);
-
-      } else if (e.target.classList.contains('casa')) {
+      } else if (e.target.classList.contains("casa")) {
         loadObserver(data, likeData, carData);
-
       }
       removeCar();
       removeLikes();
       removeFilters();
     });
   });
-};
+}
