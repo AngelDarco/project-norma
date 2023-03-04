c6a6cbee36b904e88ff27c8aedba504cf312630a
Angel Darco <angel92-eme@hotmail.com>
2023-03-04 00:54:05 -0500

    burger menu debugged and fixed bugs

--- botonMenu.js
+++ botonMenu.js
@@ -1,21 +1,23 @@
-export default function botonMenu(menuShow){
-const header = document.querySelector(".header");
-
-    const observer = new ResizeObserver((e)=>{
-      if(e[0].contentRect.width > 650){
-      menuShow.classList.remove('ocultar');
-      menu.classList.add('ocultar');  
-    }else{
-      menuShow.classList.add('ocultar');
-      menu.classList.remove('ocultar');    
-      menu.addEventListener('click', ()=>{
-        menu.classList.toggle('rotar');
-        menuShow.classList.toggle('ocultar');
-        menu.style.transition='1s';
-        if(carroContent.classList.contains('fa-shopping-cart')) 
-        carroContent.classList.replace('fa-shopping-cart','fa-cart-plus');
-    });
+export default function botonMenu(menuShow) {
+  const header = document.querySelector(".header");
+  const menu = document.getElementById("menu");
+  const observer = new ResizeObserver((e) => {
+    if (e[0].contentRect.width > 650) {
+      menuShow.classList.remove("ocultar");
+      menu.classList.add("ocultar");
+    } else {
+      menuShow.classList.add("ocultar");
+      menu.classList.remove("ocultar");
     }
-  })
+  });
+
+  observer.disconnect();
   observer.observe(header);
-};
+
+  menu.addEventListener("click", () => {
+    menu.classList.toggle("rotar");
+    menuShow.classList.toggle("ocultar");
+    menu.style.transition = ".3s";
+  });
+
+}
