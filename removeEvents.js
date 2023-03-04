eaf49f4ca155d44ef02e667b1a0ac83dd1100e9d
Angel Darco <angel92-eme@hotmail.com>
2023-03-04 00:39:07 -0500

    remove events function created

--- removeEvents.js
+++ removeEvents.js
@@ -1,28 +1,30 @@
 import eventClassNames from "./eventClassNames.js";
 
 export default function removeEvents() {
-const carroContent = document.querySelector('.carro i');
-const btnFavorites = document.querySelector('.corazon');
-const menuShow = document.querySelector(".menu__header");
-const menu = document.getElementById("menu");
-
+  const carroContent = document.querySelector(".carro i");
+  const btnFavorites = document.querySelector(".corazon");
+  const menuShow = document.querySelector(".menu__header");
+  const menu = document.getElementById("menu");
 
   function removeCar() {
-    if (!carroContent.classList.contains('like')) return;
-    eventClassNames(carroContent, 'replace', ['fa-shopping-cart', 'fa-cart-plus']);
-    eventClassNames(carroContent, 'remove', 'like')
+    if (!carroContent.classList.contains("like")) return;
+    eventClassNames(carroContent, "replace", [
+      "fa-shopping-cart",
+      "fa-cart-plus",
+    ]);
+    eventClassNames(carroContent, "remove", "like");
   }
   function removeLikes() {
-    const btnFavorites = document.querySelector('.corazon');
-    if (!btnFavorites.classList.contains('like')) return;
-    eventClassNames(btnFavorites, 'replace', ['fas', 'far']);
-    eventClassNames(btnFavorites, 'remove', 'like');
+    const btnFavorites = document.querySelector(".corazon");
+    if (!btnFavorites.classList.contains("like")) return;
+    eventClassNames(btnFavorites, "replace", ["fas", "far"]);
+    eventClassNames(btnFavorites, "remove", "like");
   }
   function removeFilters() {
-    if (menuShow.classList.contains('ocultar')) return
-    eventClassNames(menu, 'remove', 'rotar')
-    eventClassNames(menuShow, 'add', 'ocultar')
-    menu.style.transition = '1s';
+    if (menuShow.classList.contains("ocultar")) return;
+    eventClassNames(menu, "remove", "rotar");
+    eventClassNames(menuShow, "add", "ocultar");
+    menu.style.transition = "1s";
   }
-  return { removeCar, removeLikes, removeFilters }
+  return { removeCar, removeLikes, removeFilters };
 }
