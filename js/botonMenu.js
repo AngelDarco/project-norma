b043217df26c896db70df2a7270edf2beec1224b
angeldarco <angel92-eme@hotmail.com>
2024-04-16 11:38:07 -0500

    :bug: menubotton refactored

--- js/botonMenu.js
+++ js/botonMenu.js
@@ -1,22 +1,23 @@
 export default function botonMenu(menuShow) {
-  const header = document.querySelector('.header');
-  const menu = document.getElementById('menu');
+  const header = document.querySelector(".header");
+  const menu = document.getElementById("menu");
+  if (menuShow instanceof HTMLElement) return;
   const observer = new ResizeObserver((e) => {
     if (e[0].contentRect.width > 650) {
-      menuShow.classList.remove('ocultar');
-      menu.classList.add('ocultar');
+      menuShow.classList.remove("ocultar");
+      menu.classList.add("ocultar");
     } else {
-      menuShow.classList.add('ocultar');
-      menu.classList.remove('ocultar');
+      menuShow.classList.add("ocultar");
+      menu.classList.remove("ocultar");
     }
   });
 
   observer.disconnect();
   observer.observe(header);
 
-  menu.addEventListener('click', () => {
-    menu.classList.toggle('rotar');
-    menuShow.classList.toggle('ocultar');
-    menu.style.transition = '.3s';
+  menu.addEventListener("click", () => {
+    menu.classList.toggle("rotar");
+    menuShow.classList.toggle("ocultar");
+    menu.style.transition = ".3s";
   });
 }
